aec473df103ee8ebb2e9a031c07a43c9f541959d
Skira01 <jeremrichard31620@gmail.com>
2023-04-21 14:18:09 +0200

    debut

---
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test() {
+		assertEquals(FizzBuzz.fizzbuzz(1),"1");
+	}
+
+}
